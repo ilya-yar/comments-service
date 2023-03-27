47c2e28022d8c8948762fa645d693451f631d992
ilya.ovchinnikov <ilya.ovchinnikov@advcake.com>
2023-03-27 22:35:06 +0300

    Comment table and model

---
+++ app/common/models/Comment.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+/**
+ * Comment model
+ *
+ * @property integer $id
+ * @property string $subject
+ * @property integer $subject_id
+ * @property string $username
+ * @property string $comment
+ * @property string $status
+ * @property string $useragent
+ * @property string $ip
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Comment extends ActiveRecord
+{
+    const STATUS_DELETED = 0;
+    const STATUS_INACTIVE = 9;
+    const STATUS_ACTIVE = 10;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%comment}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['status', 'default', 'value' => self::STATUS_INACTIVE],
+            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_DELETED]],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+}
---
+++ app/console/migrations/m230326_193259_create_table_comment.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230326_193259_create_table_comment
+ */
+class m230326_193259_create_table_comment extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%comment}}', [
+            'id' => $this->primaryKey(),
+            'subject' => $this->string()->notNull(),
+            'subject_id' => $this->integer()->notNull(),
+            'username' => $this->string(),
+            'comment' => $this->text()->notNull(),
+            'status' => $this->string()->notNull(),
+            'useragent' => $this->string()->notNull(),
+            'ip' => $this->string()->notNull(),
+            'created_at' => $this->timestamp()->notNull(),
+            'updated_at' => $this->timestamp()->notNull(),
+        ]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%comment}}');
+    }
+}
